d2d91ee8ac15baf8c91cd8f1c7c24ec66aafe972
GabrielCabas <gabrielcabas123@gmail.com>
2024-03-06 18:44:01 -0300

    chord

--- frontend/src/components/activities/chord.tsx
+++ frontend/src/components/activities/chord.tsx
@@ -1,11 +1,12 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import ChordChart from "../plots/chord_chart"
+import config from "../../config.json";
 export default function Chord(){
     const [ data, setData ] = useState([])
     const get_data = async () => {
         try{
-            const response = await axios.get("/api/get_chord")
+            const response = await axios.get(config.activities.chord_api)
             setData(response.data.results.data)
         }
         catch(error){

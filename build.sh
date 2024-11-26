1f414026dc788734753dc76c434a79937d50a130
Diego Alvarez S. <dialvarezs@gmail.com>
2024-11-26 20:30:21 -0300

    Reorganize

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-date=$(date '+%Y%m%d')
-
-podman build -t peptipedia_backend:$date -t peptipedia_backend:latest backend/
-podman build -t peptipedia_frontend:$date -t peptipedia_frontend:latest frontend/

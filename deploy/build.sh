993d4bf9c2fb37d65c1d1a06df129454c2d73347
Diego Alvarez S. <dialvarezs@gmail.com>
2024-11-29 12:09:41 -0300

    Pull newer images on build

--- deploy/build.sh
+++ deploy/build.sh
@@ -2,5 +2,5 @@
 
 date=$(date '+%Y%m%d')
 
-podman build -t peptipedia_backend:"$date" -t peptipedia_backend:latest ../backend/
-podman build -t peptipedia_frontend:"$date" -t peptipedia_frontend:latest ../frontend/
+podman build --pull=newer -t peptipedia_backend:"$date" -t peptipedia_backend:latest ../backend/
+podman build --pull-newer -t peptipedia_frontend:"$date" -t peptipedia_frontend:latest ../frontend/

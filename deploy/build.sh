5a3dfc66715f3a633a9235968fad0760345f672b
Diego Alvarez S. <dialvarezs@gmail.com>
2024-11-29 23:46:21 -0300

    Fix

--- deploy/build.sh
+++ deploy/build.sh
@@ -3,4 +3,4 @@
 date=$(date '+%Y%m%d')
 
 podman build --pull=newer -t peptipedia_backend:"$date" -t peptipedia_backend:latest ../backend/
-podman build --pull-newer -t peptipedia_frontend:"$date" -t peptipedia_frontend:latest ../frontend/
+podman build --pull=newer -t peptipedia_frontend:"$date" -t peptipedia_frontend:latest ../frontend/

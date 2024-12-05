bf2184169235aec5aabbcb8b632eae3467035da0
Diego Alvarez S. <dialvarezs@gmail.com>
2024-12-04 22:53:10 -0300

    feat: Improve build script

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 
+build_component() {
+  local component=$1
+  echo "Building $component..."
+  podman build --pull=newer -t peptipedia_"$component":"$date" -t peptipedia_"$component":latest ../"$component"/
+}
+
+component="${1:-all}"
 date=$(date '+%Y%m%d')
 
-podman build --pull=newer -t peptipedia_backend:"$date" -t peptipedia_backend:latest ../backend/
-podman build --pull=newer -t peptipedia_frontend:"$date" -t peptipedia_frontend:latest ../frontend/
+case "$component" in
+  all)
+    build_component "backend"
+    build_component "frontend"
+    ;;
+  backend)
+    build_component "backend"
+    ;;
+  frontend)
+    build_component "frontend"
+    ;;
+  *)
+    echo "Uso: $0 [all|frontend|backend]"
+    exit 1
+    ;;
+esac
